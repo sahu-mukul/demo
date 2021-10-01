f65f43f08d0ccf5208e93057b21e14c045b84aae
sahu-mukul <54778525+sahu-mukul@users.noreply.github.com>
2021-10-01 16:28:46 +0530

    Add files via upload

---
+++ demolombook/src/main/java/com/lombook/demolombook/DemolombookApplication.java
@@ -0,0 +1,14 @@
+package com.lombook.demolombook;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DemolombookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemolombookApplication.class, args);
+	}
+}
